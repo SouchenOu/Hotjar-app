45dd38984ba820f7c989e7bf05815be4672fd8ea
SouchenOu <soukainaouchenuai@gmail.com>
2024-11-21 07:21:30 -0500

    final

--- serverSide/Controllers/Site.js
+++ serverSide/Controllers/Site.js
@@ -21,6 +21,7 @@ export const verifyTracking = async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: false,
+      executablePath: '/usr/bin/google-chrome-stable',
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
