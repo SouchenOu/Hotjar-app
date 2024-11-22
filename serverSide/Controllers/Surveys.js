3924a39f2fef35b08abe3396167ccfc240a84652
SouchenOu <soukainaouchenuai@gmail.com>
2024-11-22 09:36:36 -0500

    finish

--- serverSide/Controllers/Surveys.js
+++ serverSide/Controllers/Surveys.js
@@ -596,7 +596,6 @@ export const updateLogo = async (req, res) => {
       if (error) {
         return res.status(500).send({ error: 'Error uploading to Cloudinary' });
       }
-      console.log('Cloudinary upload result:', result);
 
       const logoUrl = result.secure_url;
 
