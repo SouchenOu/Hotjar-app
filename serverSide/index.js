19ea96e7f3088b67d9f12bfc197e4eb7256881f4
SouchenOu <soukainaouchenuai@gmail.com>
2024-11-22 08:52:20 -0500

    finish

--- serverSide/index.js
+++ serverSide/index.js
@@ -24,9 +24,7 @@ dotenv.config();
 // CORS configuration: Allow multiple origins
 const allowedOrigins = [
   'https://sitewebb-hotjarr.netlify.app',  // First frontend domain
-  'https://localhost:3000',
   'https://testsouchen-testt.netlify.app',      // Second frontend domain for the survey
-  'https://website-testtt.netlify.app',   // Add the new frontend domain
 
 ];
 
