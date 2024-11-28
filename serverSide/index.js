05bea3dc7caf91d6a1d24766c440b68dd455e2d0
SouchenOu <soukainaouchenuai@gmail.com>
2024-11-28 12:10:45 -0500

    finish

--- serverSide/index.js
+++ serverSide/index.js
@@ -22,23 +22,22 @@ const app = express();
 dotenv.config();
 
 const allowedOrigins = [
-  `${process.env.surveysDomain}`,
-  `https://solo-portfolioo.netlify.app`,
-  `${process.env.AvitoDomain}`,
+'https://websiteeehotjarrr.netlify.app/',
+'https://solo-portfolioo.netlify.app/',
 ];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
+origin: (origin, callback) => {
+if (allowedOrigins.includes(origin) || !origin) {
+callback(null, true);
+} else {
+callback(new Error('Not allowed by CORS'));
+}
+},
+methods: ['GET', 'POST', 'PUT', 'DELETE'],
+allowedHeaders: ['Content-Type', 'Authorization'],
+credentials: true,
+preflightContinue: false,
 };
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -47,12 +46,12 @@ app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB', error);
-  }
+try {
+await mongoose.connect(process.env.MONGO);
+console.log('Connected to MongoDB');
+} catch (error) {
+console.error('Error connecting to MongoDB', error);
+}
 };
 
 app.use(cookieParser());
@@ -72,36 +71,38 @@ app.options('*', cors(corsOptions)); // Automatically handle all OPTIONS request
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: allowedOrigins,
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['my-custom-header'],
-    credentials: true,
-  },
+cors: {
+origin: allowedOrigins,
+methods: ['GET', 'POST'],
+allowedHeaders: ['my-custom-header'],
+credentials: true,
+},
 });
 
 const userSockets = {};
 
 io.on('connection', (socket) => {
-  socket.on('registerUser', (userId) => {
-    userSockets[userId] = socket.id;
-  });
+socket.on('registerUser', (userId) => {
+userSockets[userId] = socket.id;
+});
 
-  socket.on('sendInvite', (inviteData) => {
-    const { recipientId, message } = inviteData;
-    const recipientSocketId = userSockets[recipientId];
+socket.on('sendInvite', (inviteData) => {
+const { recipientId, message } = inviteData;
+const recipientSocketId = userSockets[recipientId];
 
-    if (recipientSocketId) {
-      io.to(recipientSocketId).emit('inviteNotification', recipientId, message);
-    }
-  });
+if (recipientSocketId) {
+io.to(recipientSocketId).emit('inviteNotification', recipientId, message);
+} else {
+console.log(`User ${recipientId} is not connected.`);
+}
+});
 
-  socket.on('disconnect', () => {});
+socket.on('disconnect', () => {});
 });
 
 server.listen(8000, () => {
-  connect();
-  console.log(
-    'Connected to backend and Socket.IO server listening on port 8000'
-  );
+connect();
+console.log(
+'Connected to backend and Socket.IO server listening on port 8000'
+);
 });
